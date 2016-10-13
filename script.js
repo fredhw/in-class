2f4073165524215429b8a7c6f5e7b2bb65ffe756
Frederick Wijaya <fredhw@uw.edu>
2016-10-13 16:41:11 -0700

    pew

---
+++ script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+console.log("Hello, world!");
+
+var helloWorld = "Hello, world! 2";
+
+console.log(helloWorld);
+
+var x;
+
+x = 7;
+
+console.log(typeof x);
+
+var foo = null;
+
+console.log(null);
+
+
+console.log(helloWorld + x);
